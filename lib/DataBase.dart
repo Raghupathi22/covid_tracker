b25a19d22ce9d6bf71e62a19f8ccdb4e7a6b7d79
Rahul <rahulharidas06@gmail.com>
2020-09-25 13:50:04 +0530

    Updated FAQ's

--- lib/DataBase.dart
+++ lib/DataBase.dart
@@ -65,6 +65,12 @@ class DataSource {
           "Are antibiotics effective in preventing or treating the COVID-19?",
       "answer":
           "No. Antibiotics do not work against viruses, they only work on bacterial infections. COVID-19 is caused by a virus, so antibiotics do not work. Antibiotics should not be used as a means of prevention or treatment of COVID-19. They should only be used as directed by a physician to treat a bacterial infection. "
+    },
+    {
+      "question":
+      "How can I protect myself and others when using disinfectants?",
+      "answer":
+      "Disinfectant solutions should always be prepared in well-ventilated areas. Wash your hands after using any disinfectant, including surface wipes."
     }
   ];
 }
